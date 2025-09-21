e300125db63d91ad1a2ab430361dda07a4c49863
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-09-21 23:57:34 +0530

    another commit

---
+++ apps/bank_webhook_handler/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+import {z} from 'zod';
+
+app.post('/', (req, res) => {
+      
+      const paymentInformation  = {
+           token: req.body.token,
+           userId: req.body.userId,
+           amount: req.body.amount
+      }
+});
