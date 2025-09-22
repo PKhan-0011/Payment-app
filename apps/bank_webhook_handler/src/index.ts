91706d23b97c01420aec0f24492cb107d00a2699
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-09-23 01:40:39 +0530

    all code

--- apps/bank_webhook_handler/src/index.ts
+++ apps/bank_webhook_handler/src/index.ts
@@ -9,5 +9,7 @@ app.post('/', (req, res) => {
            token: req.body.token,
            userId: req.body.userId,
            amount: req.body.amount
+
+           // ye tenno data frontend s aa rhe honge okkh!..
       }
 });
